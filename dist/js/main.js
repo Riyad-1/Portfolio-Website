f901bd6b47f3d233bfaf36f66ff9b7faf35a38ac
Riyad <riyadhussain@outlook.com>
2019-07-06 15:38:33 +0100

    Third commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,12 +21,35 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  var scrollLink = $(".scroll");
+
+  // Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+      var sectionOffset = $(this.hash).offset().top - 20;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this)
+          .parent()
+          .addClass("active");
+        $(this)
+          .parent()
+          .siblings()
+          .removeClass("active");
+      }
+    });
+  });
+});
+
 // Change Current
-// $(document).on("click", "a", function() {
+// $(document).on("click", "li", function() {
 //   $(this)
-//     .addClass("current")
+//     .addClass("active")
 //     .siblings()
-//     .removeClass("current");
+//     .removeClass("active");
 // });
 
 // Smooth Scrolling
